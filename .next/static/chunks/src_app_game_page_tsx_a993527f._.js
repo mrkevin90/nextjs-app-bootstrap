30423d9eacd2b18729f8760c958b6135427e0e75
Mrkevin <167623071+mrkevin90@users.noreply.github.com>
2025-07-22 23:58:35 +0200

    Add .next/static/chunks/src_app_game_page_tsx_a993527f._.js 1753221515617

---
+++ .next/static/chunks/src_app_game_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_fde974e4._.js",
+  "static/chunks/node_modules_d39b18e0._.js"
+],
+    source: "dynamic"
+});
